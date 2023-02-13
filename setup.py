9bde3abcd9b61af836c164fb1b11b2d8073843a0
Pascal Cadotte Michaud <pcm@wazo.io>
2023-02-13 14:42:11 -0500

    add `/metrics` to chatd

--- setup.py
+++ setup.py
@@ -8,6 +8,14 @@ from setuptools import setup
 with open('wazo/plugin.yml') as file:
     metadata = yaml.load(file)
 
+flask_entry_point_path = 'metrics = wazo_prometheus_exporter.plugins.flask.metric.plugin:Plugin'
+flask_entry_point_key = [
+    'wazo_auth.http',
+    'wazo_dird.views',
+    'wazo_chatd.plugins',
+]
+flask_entry_points = {key: flask_entry_point_path for key in flask_entry_point_key}
+
 setup(
     name=metadata['name'],
     version=metadata['version'],
@@ -20,12 +28,5 @@ setup(
     package_data={
         'wazo_prometheus_exporter.plugins.flask.metric': ['api.yml'],
     },
-    entry_points={
-        'wazo_auth.http': [
-            'metrics = wazo_prometheus_exporter.plugins.flask.metric.plugin:Plugin',
-        ],
-        'wazo_dird.views': [
-            'metrics = wazo_prometheus_exporter.plugins.flask.metric.plugin:Plugin',
-        ],
-    },
+    entry_points=flask_entry_points,
 )
