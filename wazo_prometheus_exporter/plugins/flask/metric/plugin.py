e41f14aa260156338ccea9d09bbe0a7c365fcc7b
Pascal Cadotte Michaud <pcm@wazo.io>
2023-02-17 11:44:27 -0500

    Anonymize tokens in URL

--- wazo_prometheus_exporter/plugins/flask/metric/plugin.py
+++ wazo_prometheus_exporter/plugins/flask/metric/plugin.py
@@ -1,11 +1,25 @@
 # Copyright 2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import re
+
 from prometheus_flask_exporter import PrometheusMetrics
+
+TOKEN_URL_RE = re.compile(r"/token/([a-zA-Z0-9-]+)")
+ANONYMIZED_TOKEN = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+
+
+def path(req):
+    matches = TOKEN_URL_RE.search(req.path)
+    if matches:
+        token = matches.group(1)
+        return req.path.replace(token, ANONYMIZED_TOKEN)
+
+    return req.path
 
 
 class Plugin:
     def load(self, dependencies):
         api = dependencies['api']
 
-        self.metrics = PrometheusMetrics(api.app, path=f'{api.prefix}/metrics')
+        self.metrics = PrometheusMetrics(api.app, path=f'{api.prefix}/metrics', group_by=path)
