378e7a1f658aa7b6cd7c1b32498167ad7afa9de0
Pascal Cadotte Michaud <pcm@wazo.io>
2025-03-28 13:49:09 -0400

    add phoned exporter

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ flask_entry_point_key = [
     'wazo_dird.views',
     'wazo_chatd.plugins',
     'wazo_confd.plugins',
+    'wazo_phoned.plugins',
 ]
 fastapi_entry_point_key = [
     'wazo_sysconfd.plugins',
